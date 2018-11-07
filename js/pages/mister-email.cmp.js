31ae69cb9ad588e536950325d7cf25f0d5e04c49
Juval Bechar <JuvalB996@gmail.com>
2018-11-07 14:42:52 +0200

    compose email 0.5

--- js/pages/mister-email.cmp.js
+++ js/pages/mister-email.cmp.js
@@ -3,6 +3,7 @@ import emailList from '../cmps/mister-email/email-list.cmp.js';
 export default {
     template: `
     <section class="page-content">
+    <button class="compose-btn" @click="newNote">+</button>
         <email-list></email-list>
     </section>
     `,
