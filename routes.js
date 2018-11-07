894dbaed68ea9fd9ed05599eda8e9caf7a82cae2
Juval Bechar <JuvalB996@gmail.com>
2018-11-07 10:13:32 +0200

    eventbus and routes

---
+++ routes.js
@@ -0,0 +1,13 @@
+// import missKeep from ''
+// import misterEmail from ''
+
+const routes = [
+    // {path: '/', component: home},
+    // {path: '/misskeep', component: missKeep},
+    // {path: '/misteremail', component: misterEmail},
+  ];
+
+Vue.use(VueRouter);
+var myRouter = new VueRouter({routes})
+
+export default myRouter;
