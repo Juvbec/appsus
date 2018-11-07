d3f38ef549ce1275c242787b0196590378e8c813
Juval Bechar <JuvalB996@gmail.com>
2018-11-07 18:08:38 +0200

    hello/

--- js/pages/mister-email/email-details.cmp.js
+++ js/pages/mister-email/email-details.cmp.js
@@ -1,8 +1,24 @@
+import emailService from '../../services/mister-email/email.sevice.js'
 
 export default {
     template: `
-        <section class="page-content email-details">
-            <h1>hello</h1>
+        <section class="page-content email-details" v-if="email">
+            <h1>{{email.subject}}</h1>
+            <pre>{{email.message}}</pre>
         </section>
-    `
+    `,
+    data() {
+        return {
+            email: null
+        }
+    },
+    created() {
+        let emailId =  this.$route.params.emailId;
+        emailService.getById(emailId)
+        .then(email=>{
+            this.email = email;
+            console.log(email);
+            
+        })
+    }
 }
