9191c2d13275f31df12310e10bcaffd137231050
OrpazL <orpaz.lazar@gmail.com>
2018-11-07 14:55:16 +0200

    new-note creation

--- js/cmps/miss-keep/new-note.cmp.js
+++ js/cmps/miss-keep/new-note.cmp.js
@@ -4,15 +4,17 @@ import noteService from '../../services/miss-keep/notes-service.js';
 export default {
     template: `
         <section class="new-note-container">
-            <div class="screen-blur" @click="this.$emit('closeModal')"></div>
-            <div class="new-note-modal">
-                <form @submit.prevent="" class="new-note-form">
-                    <input type="text" placeholder="Title" v-model="note.title" />
-                    <textarea ref="content" placeholder="Take a note..." v-model="note.content"></textarea>
-                </form>
-                <div class="note-control-panel">
-                    <i title="Discard" class="fas fa-trash"></i>
-                    <i title="Save" class="fas fa-check"></i>
+            <div class="screen-blur" @click="discardNote">
+
+                <div class="new-note-modal" @click.stop>
+                    <form @submit.prevent="" class="new-note-form">
+                        <input type="text" placeholder="Title" v-model="note.title" />
+                        <textarea ref="content" placeholder="Take a note..." v-model="note.content"></textarea>
+                    </form>
+                    <div class="note-control-panel">
+                        <i title="Discard" class="fas fa-trash" @click="discardNote"></i>
+                        <i title="Save" class="fas fa-check" @click="addNote"></i>
+                    </div>
                 </div>
             </div>
             {{note}}
@@ -25,7 +27,7 @@ export default {
                 content: '',
                 isPinned: false,
                 at: '',
-                bgColor: '',
+                bgColor: '#f5deb3',
             }
         }
     },
@@ -34,9 +36,20 @@ export default {
     },
     methods: {
         addNote() {
-            // noteService.addNote(this.note);
+            this.note.at = Date.now();
             console.log(this.note)
+            noteService.addNote(this.note);
+            this.$emit('closeModal');
         },
+        discardNote() {
+            console.log(this.note.title.trim().length , this.note.content.trim().length)
+            if (this.note.title.trim().length || this.note.content.trim().length){
+                if (!confirm('Discard note?')) return;
+                else this.$emit('closeModal');
+            } else {
+                this.$emit('closeModal');
+            }
+        }
     }
 
 }
