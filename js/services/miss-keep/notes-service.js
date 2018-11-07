21bad8aafe88c90a2db9124c0abd28d947b95231
OrpazL <orpaz.lazar@gmail.com>
2018-11-07 11:10:59 +0200

    start with note-service

---
+++ js/services/miss-keep/notes-service.js
@@ -0,0 +1,31 @@
+import storageService from './storage.service.js'
+import utilService from './util.service.js'
+
+
+const STORAGE_KEY = 'missKeepAppKey';
+
+export default {
+    query,
+    getById,
+}
+
+function query(filter = null) {
+    return storageService.load(STORAGE_KEY)
+        .then(notes => {
+            if (!notes || !notes.length) {
+                notes = [];
+                storageService.store(STORAGE_KEY, notes);
+            }
+            console.log('notes: ', notes);
+            if (filter === null) return notes;
+            else return notes.filter(note => 
+                            note.vendor.toUpperCase().includes(filter.byVendor.toUpperCase()));
+        })
+}
+
+function getById(noteId) {
+    return storageService.load(STORAGE_KEY)
+        .then(notes => {
+            return notes.find(note => note.id === noteId);
+        })
+}
