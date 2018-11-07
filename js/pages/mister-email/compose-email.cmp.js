b2aa205ea53f25157f70bfe4b772082865a5387f
Juval Bechar <JuvalB996@gmail.com>
2018-11-07 15:43:03 +0200

    gfdsgfds

--- js/pages/mister-email/compose-email.cmp.js
+++ js/pages/mister-email/compose-email.cmp.js
@@ -2,7 +2,30 @@
 export default {
     template: `
         <section class="page-content">
-            
-        </section>
-    `
+            <div class="flex">
+                <button @click="sendEmail">Send</button>
+            </div>
+            <div class="flex flex-column">
+                <label>
+                    to: 
+                    <input type="text">
+                </label>
+                <label>
+                    subject: 
+                    <input type="text">
+                </label>
+            </div>
+            <textarea placeholder="Enter message here..."></textarea>
+        </div>
+    `,
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        sendEmail() {
+            this.$router.push('/misteremail');
+        }
+    }
 }
--- js/services/mister-email/email.sevice.js
+++ js/services/mister-email/email.sevice.js
@@ -2,44 +2,27 @@ import storageService from '../storage.service.js'
 import utilService from '../util.service.js'
 
 
-const STORAGE_KEY = 'misterEmailAppKey';
+const STORAGE_KEY = 'missKeepAppKey';
 
 export default {
     query,
     getById,
-    addEmail
+    addEmail,
+    saveEmail
 }
-// filter = null
-function query() {
-    // return storageService.load(STORAGE_KEY)
-    //     .then(emails => {
-    //         if (!emails || !emails.length) {
-    //             emails = [];
-    //             storageService.store(STORAGE_KEY, emails);
-    //         }
-    //         console.log('emails: ', emails);
-    //         if (filter === null) return emails;
-    //         else return emails.filter(email => 
-    //                         email.vendor.toUpperCase().includes(filter.byVendor.toUpperCase()));
-    //     })
-    var emails = [
-        {
-            id: utilService.makeId(),
-            subject: 'im juval',
-            from: 'juval',
-            to: 'orpaz',
-            txt: 'gjdsfklgjdfksl'
-        },
-        {
-            id: utilService.makeId(),
-            subject: 'im orpaz',
-            from: 'orpaz',
-            to: 'juval',
-            txt: 'AAAAAAAAAAAAAa'
-        },
-        
-    ]
-    return Promise.resolve(emails);
+
+function query(filter = null) {
+    return storageService.load(STORAGE_KEY)
+        .then(emails => {
+            if (!emails || !emails.length) {
+                emails = [];
+                storageService.store(STORAGE_KEY, emails);
+            }
+            console.log('emails: ', emails);
+            if (filter === null) return emails;
+            else return emails.filter(email => 
+                            email.vendor.toUpperCase().includes(filter.byVendor.toUpperCase()));
+        })
 }
 
 function getById(emailId) {
@@ -49,6 +32,24 @@ function getById(emailId) {
         })
 }
 
-function addEmail(newNote) {
+function addEmail(newEmail) {
+    // console.log(newNote);
+    newEmail.id = utilService.makeId();
+    saveEmail(newEmail);
+}
 
+function saveEmail(email) {
+    return storageService.load(STORAGE_KEY)
+        .then(emails => {
+            // Update
+            if (email.id) {
+                var emailIdx = emails.findIndex(currEmail => currEmail.id === email.id)
+                emails.splice(emailIdx, 1, email);
+            } else {
+                // Add
+                email.id = utilService.makeId();
+                emails.push(email);
+            }
+            return storageService.store(STORAGE_KEY, emails);
+        });
 }
