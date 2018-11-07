efdfc7dce03c3ceca2e5ef0533668d0fed334007
OrpazL <orpaz.lazar@gmail.com>
2018-11-07 11:48:31 +0200

    some service and note-list.css

--- js/cmps/miss-keep/notes-list.cmp.js
+++ js/cmps/miss-keep/notes-list.cmp.js
@@ -4,8 +4,14 @@ export default {
     props: ['notes'],
     template: `
         <section class="notes-list-container">
+            <button class="new-note-btn" @click="newNote">+</button>
             <note-preview v-for="(note, idx) in notes" :note="note"></note-preview>
         </section>
     `,
+    methods: {
+        newNote() {
+            this.$emit('newNote');
+        }
+    }
 
 }
