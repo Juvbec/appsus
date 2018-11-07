4f62c7a931599dee219ee6b18e1c2ec86bc737cf
Juval Bechar <JuvalB996@gmail.com>
2018-11-07 10:56:47 +0200

    home page??

--- js/pages/home-page.cmp.js
+++ js/pages/home-page.cmp.js
@@ -2,8 +2,10 @@
 
 export default {
     template: `
-    <section class="page-content">
-        <h1>home page</h1>
+    <section class="page-content home-page">
+        <div class="home-page-link">MisterEmail</div>
+        <div class="home-page-link">MissKeep</div>
     </section>
-    `
+    `,
+    
 }
