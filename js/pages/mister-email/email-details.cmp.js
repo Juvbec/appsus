631061efbc6e63c593c29fe52890005a34c351ee
Juval Bechar <JuvalB996@gmail.com>
2018-11-10 22:00:32 +0200

    delete detail button

--- js/pages/mister-email/email-details.cmp.js
+++ js/pages/mister-email/email-details.cmp.js
@@ -1,5 +1,5 @@
 import emailService from '../../services/mister-email/email.sevice.js'
-import eventBus, {DETAILED_VIEW, UPDATE_EMAIL} from '../../services/event-bus.service.js';
+import eventBus, { DETAILED_VIEW, UPDATE_EMAIL, EMAIL_CHANGE } from '../../services/event-bus.service.js';
 
 export default {
     template: `
@@ -7,6 +7,7 @@ export default {
             <nav>
                 <button class="email-btn" @click="goBack">Go Back</button>
                 <button class="email-btn" @click="onReply">Reply</button>
+                <i class="email-btn fas fa-trash" @click="deleteEmail"></i>
             </nav>
             <div class="details-container">
                 <h1>{{email.subject}}</h1>
@@ -27,27 +28,34 @@ export default {
             this.$router.push('/misteremail');
         },
         loadEmail() {
-            let emailId =  this.$route.params.emailId;
-        return emailService.getById(emailId)
-        .then(email=>{
-            this.email = email;    
-            this.email.isRead = true;
-            eventBus.$emit(UPDATE_EMAIL, this.email)
-        })
+            let emailId = this.$route.params.emailId;
+            return emailService.getById(emailId)
+                .then(email => {
+                    this.email = email;
+                    this.email.isRead = true;
+                    eventBus.$emit(UPDATE_EMAIL, this.email)
+                })
         },
         onReply() {
-            this.$router.push(`/misteremail/compose/${this.email.id}`);            
+            this.$router.push(`/misteremail/compose/${this.email.id}`);
+        },
+        deleteEmail() {
+            emailService.deleteEmail(this.$route.params.emailId)
+                .then(() => {
+                    eventBus.$emit(EMAIL_CHANGE);
+                    this.$router.push(`/misteremail`);
+                })
         }
     },
     created() {
         console.log('details created!');
         this.loadEmail()
-        .then(()=> {
-            eventBus.$on(DETAILED_VIEW, this.loadEmail)
-        })
+            .then(() => {
+                eventBus.$on(DETAILED_VIEW, this.loadEmail)
+            })
     },
     destroyed() {
         console.log('details destroyed');
-        
+
     }
 } 
