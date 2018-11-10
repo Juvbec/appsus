e2ef8af193b503e6d0ce3fd4c3552cb235c525b3
OrpazL <orpaz.lazar@gmail.com>
2018-11-10 12:34:56 +0200

    modified long-press menu

--- js/cmps/miss-keep/note-menu.cmp.js
+++ js/cmps/miss-keep/note-menu.cmp.js
@@ -1,4 +1,6 @@
-import eventBus, { DELETE_NOTE } from '../../services/event-bus.service.js';
+import eventBus, {NOTES_CHANGE, DELETE_NOTE } from '../../services/event-bus.service.js';
+import noteService from '../../services/miss-keep/notes-service.js';
+
 
 export default {
     props: ['note'],
@@ -7,11 +9,21 @@ export default {
             <div class="bubble" @click.stop="deleteNote">
                 <i title="Discard" class="fas fa-trash"></i>   
             </div>
+            <div class="bubble" @click.stop="pinNote">
+                <i v-if="note.isPinned" class="fas fa-thumbtack pinned"></i>
+                <i v-else="" class="fas fa-thumbtack"></i>
+            </div>
         </section>
     `,
     methods: {
         deleteNote() {
             eventBus.$emit(DELETE_NOTE, this.note);
-        }
+        },
+        pinNote() {
+            this.note.isPinned = !this.note.isPinned;
+            noteService.saveNote(this.note).then(res => {
+                eventBus.$emit(NOTES_CHANGE);
+            });
+        },
     }
 }
