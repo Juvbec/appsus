11ce638882b7002b9bd0b119a5e5dc85a8e7e08d
Juval Bechar <JuvalB996@gmail.com>
2018-11-11 00:25:35 +0200

    unread

--- js/pages/mister-email/email-details.cmp.js
+++ js/pages/mister-email/email-details.cmp.js
@@ -5,9 +5,17 @@ export default {
     template: `
         <section class="mister-email-content email-details" v-if="email">
             <nav>
+                <div>
                 <button class="email-btn" @click="goBack">Go Back</button>
                 <button class="email-btn" @click="onReply">Reply</button>
+                </div>
+                <div>
+                <template v-if="email">
+                <button class="email-btn" v-if="email.isRead" @click="onMarkAsUnread">Mark as Unread</button>
+                <button class="email-btn" v-else @click="onMarkAsUnread">Mark as read</button>
+                </template>
                 <i class="email-btn fas fa-trash" @click="deleteEmail"></i>
+                </div>
             </nav>
             <div class="details-container">
                 <h1>{{email.subject}}</h1>
@@ -39,6 +47,13 @@ export default {
         onReply() {
             this.$router.push(`/misteremail/compose/${this.email.id}`);
         },
+        onMarkAsUnread() {
+            this.email.isRead = !this.email.isRead;
+            emailService.saveEmail(this.email)
+            .then(()=> {
+                eventBus.$emit(EMAIL_CHANGE);
+            });
+        },
         deleteEmail() {
             emailService.deleteEmail(this.$route.params.emailId)
                 .then(() => {
@@ -48,14 +63,9 @@ export default {
         }
     },
     created() {
-        console.log('details created!');
         this.loadEmail()
             .then(() => {
                 eventBus.$on(DETAILED_VIEW, this.loadEmail)
             })
     },
-    destroyed() {
-        console.log('details destroyed');
-
-    }
 } 
